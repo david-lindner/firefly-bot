42510eefa7dc72269c5065922156e2199c35b771
David Lindner <dev@davidlindner.me>
2020-04-29 22:39:58 +0200

    reformatting

--- src/bot.py
+++ src/bot.py
@@ -10,71 +10,82 @@ import os
 from pathlib import Path
 
 from firefly import Firefly
-from telegram import (InlineKeyboardButton, InlineKeyboardMarkup,
-                      ReplyKeyboardRemove)
-from telegram.ext import (CallbackQueryHandler, CommandHandler,
-                          ConversationHandler, Filters, MessageHandler,
-                          PicklePersistence, Updater)
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    CallbackQueryHandler,
+    CommandHandler,
+    ConversationHandler,
+    Filters,
+    MessageHandler,
+    PicklePersistence,
+    Updater,
+)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
 logger = logging.getLogger(__name__)
 
 FIREFLY_URL, FIREFLY_TOKEN, DEFAULT_WITHDRAW_ACCOUNT = range(3)
 
 
 def start(update, context):
-    update.message.reply_text('Hi! Please enter your Firefly III URL')
+    update.message.reply_text("Hi! Please enter your Firefly III URL")
     return FIREFLY_URL
 
 
 def get_firefly_token(update, context):
-    context.user_data['firefly_url'] = update.message.text
-    update.message.reply_text('Hi! Please enter your Firefly III User Token')
+    context.user_data["firefly_url"] = update.message.text
+    update.message.reply_text("Hi! Please enter your Firefly III User Token")
     return DEFAULT_WITHDRAW_ACCOUNT
 
 
 def get_default_account(update, context):
     token = update.message.text
-    firefly = Firefly(hostname=context.user_data.get(
-        "firefly_url"), auth_token=token)
+    firefly = Firefly(hostname=context.user_data.get("firefly_url"), auth_token=token)
     accounts = firefly.get_accounts(account_type="asset").get("data")
 
     accounts_keyboard = []
     for account in accounts:
         account_name = account.get("attributes").get("name")
-        accounts_keyboard.append([InlineKeyboardButton(
-            account_name, callback_data=account.get("id"))])
+        accounts_keyboard.append(
+            [InlineKeyboardButton(account_name, callback_data=account.get("id"))]
+        )
 
     reply_markup = InlineKeyboardMarkup(accounts_keyboard)
 
-    context.user_data['firefly_token'] = update.message.text
-    update.message.reply_text('Please choose:', reply_markup=reply_markup)
+    context.user_data["firefly_token"] = update.message.text
+    update.message.reply_text("Please choose:", reply_markup=reply_markup)
     return DEFAULT_WITHDRAW_ACCOUNT
 
 
 def store_default_account(update, context):
     query = update.callback_query
     default_account_id = query.data
-    context.user_data['firefly_default_account'] = default_account_id
-    query.edit_message_text('Setup Complete. Happy Spending!(?)')
+    context.user_data["firefly_default_account"] = default_account_id
+    query.edit_message_text("Setup Complete. Happy Spending!(?)")
     return ConversationHandler.END
 
+
 def spend(update, context):
-    message = update.message.text.split(' ')
+    message = update.message.text.split(" ")
 
     if len(message) < 2:
-        update.message.reply_text("Just type in an expense with a description. Like this - '5 Starbucks`")
+        update.message.reply_text(
+            "Just type in an expense with a description. Like this - '5 Starbucks`"
+        )
         return
 
     amount = message[0]
     description = message[1]
-    category = message[2] if 2 < len(message) else None # Safely try to check for index
-    budget = message[3] if 3 < len(message) else None # Safely try to check for index
-    
+    category = message[2] if 2 < len(message) else None  # Safely try to check for index
+    budget = message[3] if 3 < len(message) else None  # Safely try to check for index
+
     firefly = get_firefly(context)
     account = context.user_data["firefly_default_account"]
-    response = firefly.create_transaction(amount, description, account, category, budget)
+    response = firefly.create_transaction(
+        amount, description, account, category, budget
+    )
     if response.status_code == 422:
         update.message.reply_text(response.get("message"))
     elif response.status_code == 200:
@@ -83,27 +94,32 @@ def spend(update, context):
         update.message.reply_text("Something went wrong, check logs")
 
 
-
 def about(update, context):
     firefly = get_firefly(context)
     about = firefly.get_about_user()
     update.message.reply_text("```{}```".format(about))
 
+
 def get_firefly(context):
-    return Firefly(hostname=context.user_data.get("firefly_url"), auth_token=context.user_data.get("firefly_token"))
+    return Firefly(
+        hostname=context.user_data.get("firefly_url"),
+        auth_token=context.user_data.get("firefly_token"),
+    )
 
 
 def help(update, context):
-    if not context.user_data.get('firefly_default_account'):
+    if not context.user_data.get("firefly_default_account"):
         update.message.reply_text("Type /start to initiate the setup process.")
     else:
-        update.message.reply_text("""Just type in an expense with a description. \Like this - 
+        update.message.reply_text(
+            """Just type in an expense with a description. \Like this - 
         \n '5 Starbucks' \n Additionally you can also include the category and budget (both optional)
-        \n '5 Starbucks Coffee Food'""")
+        \n '5 Starbucks Coffee Food'"""
+        )
 
 
 def cancel(update, context):
-    update.message.reply_text('Cancelled')
+    update.message.reply_text("Cancelled")
     return ConversationHandler.END
 
 
@@ -117,24 +133,27 @@ def main():
     if not data_dir:
         data_dir = Path.joinpath(Path.home(), ".config", "firefly-bot")
         data_dir.mkdir(parents=True, exist_ok=True)
-    bot_persistence = PicklePersistence(filename=str(data_dir/"bot-data"))
+    bot_persistence = PicklePersistence(filename=str(data_dir / "bot-data"))
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
-    updater = Updater(bot_token,
-                      persistence=bot_persistence, use_context=True)
+    updater = Updater(bot_token, persistence=bot_persistence, use_context=True)
 
     conversation_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', start)],
+        entry_points=[CommandHandler("start", start)],
         states={
             FIREFLY_URL: [MessageHandler(Filters.text, get_firefly_token)],
-            DEFAULT_WITHDRAW_ACCOUNT: [MessageHandler(Filters.text, get_default_account),
-                                       CallbackQueryHandler(store_default_account, pattern='^[0-9]*$')]
+            DEFAULT_WITHDRAW_ACCOUNT: [
+                MessageHandler(Filters.text, get_default_account),
+                CallbackQueryHandler(store_default_account, pattern="^[0-9]*$"),
+            ],
         },
-        fallbacks=[CommandHandler('cancel', cancel)]
+        fallbacks=[CommandHandler("cancel", cancel)],
     )
 
-    updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.dispatcher.add_handler(CommandHandler('about', about))
-    updater.dispatcher.add_handler(MessageHandler(filters=Filters.regex("^[0-9]+"), callback=spend))
+    updater.dispatcher.add_handler(CommandHandler("help", help))
+    updater.dispatcher.add_handler(CommandHandler("about", about))
+    updater.dispatcher.add_handler(
+        MessageHandler(filters=Filters.regex("^[0-9]+"), callback=spend)
+    )
     updater.dispatcher.add_error_handler(error)
     updater.dispatcher.add_handler(conversation_handler)
 
@@ -146,5 +165,5 @@ def main():
     updater.idle()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/firefly.py
+++ src/firefly.py
@@ -1,40 +1,49 @@
 import requests
 import datetime
 
+
 class Firefly(object):
     def __init__(self, hostname, auth_token):
-        self.headers = {'Authorization': "Bearer " + auth_token}
+        self.headers = {"Authorization": "Bearer " + auth_token}
         self.hostname = hostname + "/api/v1/"
-    
+
     def _post(self, endpoint, payload):
-        return requests.post("{}{}".format(self.hostname, endpoint), json=payload, headers=self.headers)
+        return requests.post(
+            "{}{}".format(self.hostname, endpoint), json=payload, headers=self.headers
+        )
 
     def _get(self, endpoint, params=None):
-        response = requests.get("{}{}".format(self.hostname, endpoint), params=params, headers=self.headers)
+        response = requests.get(
+            "{}{}".format(self.hostname, endpoint), params=params, headers=self.headers
+        )
         return response.json()
 
     def get_budgets(self):
         return self._get("budgets")
-    
+
     def get_accounts(self, account_type="asset"):
-        return self._get("accounts", params={"type" : account_type})
+        return self._get("accounts", params={"type": account_type})
 
     def get_about_user(self):
         return self._get("about/user")
 
-    def create_transaction(self, amount, description, account, category=None, budget=None):
+    def create_transaction(
+        self, amount, description, account, category=None, budget=None
+    ):
         now = datetime.datetime.now()
         payload = {
-            "transactions": [{
-                "type": "withdrawal",
-                "description": description,
-                "date": now.strftime("%Y-%m-%d"),
-                "amount": amount,
-                "source_id": account,
-                "destination_name": description,
-                "budget_name": budget,
-                "category_name": category,
-            }]
+            "transactions": [
+                {
+                    "type": "withdrawal",
+                    "description": description,
+                    "date": now.strftime("%Y-%m-%d"),
+                    "amount": amount,
+                    "source_id": account,
+                    "destination_name": description,
+                    "budget_name": budget,
+                    "category_name": category,
+                }
+            ]
         }
-        
+
         return self._post(endpoint="transactions", payload=payload)
